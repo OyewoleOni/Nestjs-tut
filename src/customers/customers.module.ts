1decedbf084767a8ed952b2f0760acb5e616cda8
Oyewole Oni <onioyewole1910@gmail.com>
2022-12-21 10:07:58 +0100

    Middlware Implementatiom 02

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,5 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 import { CustomersController } from './controllers/customers/customers.controller';
+import { ValidateCustomerAccountMiddleware } from './middlewares/validate-customer-account.middleware';
 import { ValidateCustomerMiddleware } from './middlewares/validate-customer.middleware';
 import { CustomersService } from './services/customers/customers.service';
 
@@ -10,10 +12,18 @@ import { CustomersService } from './services/customers/customers.service';
 })
 export class CustomersModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(ValidateCustomerMiddleware).forRoutes({
-      path: 'customers/search/:id',
-      method: RequestMethod.GET
+    consumer.apply(ValidateCustomerMiddleware, ValidateCustomerAccountMiddleware, (
+      req: Request, 
+      res: Response, 
+      next: NextFunction) => {
+        console.log('Last Middleware')
+        next();
     })
+        .exclude({
+          path: 'api/customers',
+          method: RequestMethod.POST
+        })
+        .forRoutes(CustomersController)
   }
 
 }
